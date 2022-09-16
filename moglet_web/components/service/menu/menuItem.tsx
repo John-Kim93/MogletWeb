5e31179bb1c864e3d5d525dd0398bc516e342548
John-Kim93 <dhkdhd123@naver.com>
2022-09-16 18:06:44 +0900

    FEAT table UI

--- moglet_web/components/service/menu/menuItem.tsx
+++ moglet_web/components/service/menu/menuItem.tsx
@@ -15,40 +15,41 @@ export default function MenuItem({ menu }) {
   const deleteMenu = useMutation((uid :number) => apiDeleteMenu(uid))
   if (!editMenu) {
     return (
-      <div key={menu.uid}>
-        <h4>메뉴명 : {menu.name}</h4>
-        <h4>메뉴 소개 : {menu.content}</h4>
-        <h4>가격 : {menu.price}</h4>
-        <h4>is_main : {menu.is_main}</h4>
-        <button
-          onClick={() => deleteMenu.mutate(menu.uid, {
-            onSuccess: () => { // 요청이 성공한 경우
-              // 알람 띄우기
-              Swal.fire({
-                position: 'top-end',
-                icon: 'success',
-                title: '메뉴가 삭제되었습니다.',
-                showConfirmButton: false,
-                timer: 2000
-              })
-              queryClient.invalidateQueries('get_menu')
-            },
-            onError: (error) => { // 요청에 에러가 발생된 경우
-              console.log('onError' + error);
-            },
-            onSettled: () => { // 요청이 성공하든, 에러가 발생되든 실행하고 싶은 경우
-            }
-          })}
-        >
-          메뉴 삭제
-        </button>
-        <button
-          onClick={() => setEditMenu(!editMenu)}
-        >
-          메뉴 수정
-        </button>
-        <hr/>
-      </div>
+      <tr key={menu.uid}>
+        <td>{menu.name}</td>
+        <td>{menu.content}</td>
+        <td className="textAlignCenter">₩{menu.price}</td>
+        <td className="textAlignCenter">{menu.is_main ? 'O' : ''}</td>
+        <td>
+          <button
+            onClick={() => deleteMenu.mutate(menu.uid, {
+              onSuccess: () => { // 요청이 성공한 경우
+                // 알람 띄우기
+                Swal.fire({
+                  position: 'top-end',
+                  icon: 'success',
+                  title: '메뉴가 삭제되었습니다.',
+                  showConfirmButton: false,
+                  timer: 2000
+                })
+                queryClient.invalidateQueries('get_menu')
+              },
+              onError: (error) => { // 요청에 에러가 발생된 경우
+                console.log('onError' + error);
+              },
+              onSettled: () => { // 요청이 성공하든, 에러가 발생되든 실행하고 싶은 경우
+              }
+            })}
+          >
+            메뉴 삭제
+          </button>
+          <button
+            onClick={() => setEditMenu(!editMenu)}
+          >
+            메뉴 수정
+          </button>
+        </td>
+      </tr>
     )
   } else {
     const initialValues = {
--- moglet_web/components/service/menu/menuItems.tsx
+++ moglet_web/components/service/menu/menuItems.tsx
@@ -22,12 +22,26 @@ export default function MenuItems() :JSX.Element {
   } else {
     return (
       <div>
+        <h4>등록된 메뉴 수 : {menus.length}개</h4>
         <Link href="/service/create/menuCreate">
           <button>메뉴 추가</button>
         </Link>
-        {menus.map(menu => 
-          <MenuItem key={menu.uid} menu={menu}/>
-        )}
+        <table>
+          <thead>
+            <tr>
+              <th>메뉴명</th>
+              <th>메뉴 소개</th>
+              <th>가격</th>
+              <th>메인 메뉴</th>
+              <th>삭제 및 수정</th>
+            </tr>
+          </thead>
+          <tbody>
+            {menus.map(menu => 
+              <MenuItem key={menu.uid} menu={menu}/>
+            )}
+          </tbody>
+        </table>
       </div>
     )
   }
