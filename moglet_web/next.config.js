c833410d100262beb75abc8f4b83cbafc812a402
John-Kim93 <dhkdhd123@naver,com>
2022-10-14 21:05:57 +0900

    FIX config

--- moglet_web/next.config.js
+++ moglet_web/next.config.js
@@ -1,20 +1,19 @@
 /** @type {import('next').NextConfig} */
-import { S3_URL, API_URL, IMAGE_DOMAIN, VIDEO_DOMAIN } from "/config/index" 
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [IMAGE_DOMAIN],
+    domains: [process.env.IMAGE_DOMAIN],
   },
   swcMinify: true,
   env: {
-    S3_URL : S3_URL
+    S3_URL : process.env.S3_URL
   },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: API_URL,
+        destination: process.env.API_URL,
       },
     ]
   },
