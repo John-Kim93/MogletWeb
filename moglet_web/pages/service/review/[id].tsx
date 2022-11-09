3e50886de934336880ca1a2b04b730389b90dab1
John-Kim93 <dhkdhd123@naver,com>
2022-11-09 14:51:05 +0900

    FEAT UI/F, 돌아가기btn

--- moglet_web/pages/service/review/[id].tsx
+++ moglet_web/pages/service/review/[id].tsx
@@ -6,10 +6,13 @@ import LocalStorage from "../../../store/LocalStorage";
 import { useState, useEffect } from 'react'
 import style from "../../../styles/service/Review.module.css"
 import Image from "next/image";
+import { useRouter } from 'next/router'
 
 export default function ReviewDetail() {
   const [videoUrl, setVideoUrl] = useState('')
   const [thumbnailUrl, setThumbnailUrl] = useState('')
+
+  const router = useRouter()
 
   const reviewJSON = LocalStorage.getItem('reviewDetail')
   const review = JSON.parse(reviewJSON)
@@ -29,23 +32,26 @@ export default function ReviewDetail() {
         <div className="flexBoxStart">
           {videoUrl !== '' && <div className={style.videoBox}><VideoPlayer videoUrl={videoUrl} thumbnailUrl={thumbnailUrl}/></div>}
           <div className={style.textWrapper}>
-          <div className="flexBoxStart">
-            <h3>작성자 : {review?.nickname}</h3>
-            <div className={style.imageBox}>
-              <Image
-                src={review?.profileFilename}
-                width={50}
-                height={50}
-                unoptimized
-              />
+            <div className="flexBoxStart">
+              <h3>작성자 : {review?.nickname}</h3>
+              <div className={style.imageBox}>
+                <Image
+                  src={review?.profileFilename}
+                  width={50}
+                  height={50}
+                  unoptimized
+                />
+              </div>
             </div>
-          </div>
             <h3>작성 시간 : {review?.createdTime}</h3>
             <h3>메인 리뷰 : {review?.isMain ? "O" : "X"}</h3>
             <h3>조회수 : {review?.countView}회</h3>
             <h3>평가 : {review?.visitSatisfaction}</h3>
             <h3>리뷰 내용</h3>
             <h4 className={style?.flexBox}>{review?.content}</h4>
+            <div className="flexBoxStart" style={{ margin: '15px 0 0 -5px' }}>
+              <button className="cancel" onClick={()=>{router.back()}}>돌아가기</button>
+            </div>
           </div>
         </div>
       </div>
